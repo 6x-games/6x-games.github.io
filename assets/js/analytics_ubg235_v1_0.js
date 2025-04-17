598328aaec20b7e618144406c5d94ab899c225f2
6x-games <6xgames647@gmail.com>
2025-04-17 15:28:38 +0500

    Update analytics_ubg235_v1_0.js

--- assets/js/analytics_ubg235_v1_0.js
+++ assets/js/analytics_ubg235_v1_0.js
@@ -1,23 +1,16 @@
-function loadJS(FILE_URL, async = true) {
-  let scriptEle = document.createElement("script");
+// Load the Google Analytics gtag.js script
+(function() {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-M97CMQ0H59';
+  document.head.appendChild(script);
 
-  scriptEle.setAttribute("src", FILE_URL);
-  scriptEle.setAttribute("type", "text/javascript");
-  scriptEle.setAttribute("async", async);
+  // Initialize gtag
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function() {
+    dataLayer.push(arguments);
+  };
 
-  document.body.appendChild(scriptEle);
-
-  // Success
-  scriptEle.addEventListener("load", () => {
-    console.log("ubg235 served!");
-  });
-
-   // Error
-  scriptEle.addEventListener("error", () => {
-    console.log("ubg235 error!");
-  });
-}
-
-window.addEventListener("load", function () {
-  loadJS("https://www.ubg235.com/js/ubg235_server_v1_0.js", true);
-});
+  window.gtag('js', new Date());
+  window.gtag('config', 'G-M97CMQ0H59');
+})();
